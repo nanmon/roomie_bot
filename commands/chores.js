279bd27ed73cb6881760fd1df283e9ec1acd7631
Nan Montaño <nano.mv112@gmail.com>
2021-03-09 20:12:34 -0700

    rrule

--- commands/chores.js
+++ commands/chores.js
@@ -1,27 +1,29 @@
 const format = require('date-fns/format')
 const { es } = require('date-fns/locale')
+const { rrulestr } = require('rrule')
 
-const DAY = 1000 * 60 * 60 * 24
-const WEEK = DAY * 7;
 /**
  * 
  * @param {import('telegraf').Telegraf} bot 
  */
 const chores = (bot, commandName, conf) => {
+  const rule = rrulestr(conf.rrule);
   bot.command(commandName, ctx => {
     const args = ctx.message.text.split(' ');
     args.shift();
-    const day = new Date(...args)
-    const offIndex = Math.floor(day.valueOf() / WEEK * 2); //twice a week
-    const dayOffset = DAY * 3
-    const cleaningDay = new Date(WEEK / 2 * offIndex + dayOffset);
-    const offset = offIndex % conf.chores.length;
+    const day = new Date(...args);
+    const nextDay = rule.after(day, true);
+
+    const days = rule.between(rule.options.dtstart, nextDay, true);
+    const offset = days.length % conf.chores.length;
+    const cleaningDay = days.pop();
+
     const choreList = conf.people.map((person, index) => {
       return `${person}: ${conf.chores[(index + offset) % conf.chores.length]}`
     });
     ctx.reply(
       "Roles del " + 
-      format(cleaningDay, 'EEEE, dd LLLL', {locale: es }) + 
+      format(cleaningDay, 'EEEE, dd LLLL', { locale: es }) + 
       "\n" +
       choreList.join('\n')
     );
